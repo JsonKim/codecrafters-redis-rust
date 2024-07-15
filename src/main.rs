0f91ce951c92faf62bcdbf3df03b9830e7741fd6
Jason Kim <actcrazy@gmail.com>
2024-07-15 22:42:39 +0900

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,22 @@
-use std::net::TcpListener;
+use std::io::{Error, Write};
+use std::net::{TcpListener, TcpStream};
+
+fn handle_client(mut stream: TcpStream, message: &str) -> Result<(), Error> {
+    stream.write(message.as_bytes())?;
+    stream.flush()?;
+    Ok(())
+}
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(stream) => {
                 println!("accepted new connection");
+                if let Err(e) = handle_client(stream, "+PONG\r\n") {
+                    eprintln!("Error handling client: {}", e);
+                }
             }
             Err(e) => {
                 println!("error: {}", e);
