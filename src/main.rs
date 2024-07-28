786559e68b187e3f0640d35f587f5f97d519dbc5
Jason Kim <actcrazy@gmail.com>
2024-07-28 22:51:40 +0900

    fix: ECHO 커맨드 처리 & 구조 개선

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,13 @@ fn handle_client(mut stream: &TcpStream, message: &str) -> Result<(), Error> {
     Ok(())
 }
 
+fn buf_to_lines(buf: &[u8]) -> Vec<String> {
+    String::from_utf8_lossy(buf)
+        .lines()
+        .map(|line| line.trim_end().to_string())
+        .collect()
+}
+
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
@@ -22,8 +29,22 @@ fn main() {
                             break;
                         }
 
-                        if let Err(e) = handle_client(&stream, "+PONG\r\n") {
-                            eprintln!("Error handling client: {}", e);
+                        let lines = buf_to_lines(&buf);
+                        match lines.get(2) {
+                            Some(command) if command == "PING" => {
+                                if let Err(e) = handle_client(&stream, "+PONG\r\n") {
+                                    eprintln!("Error handling client: {}", e);
+                                }
+                            }
+                            Some(command) if command == "ECHO" => {
+                                let message = format!("+{}\r\n", lines.get(4).unwrap().to_string());
+                                if let Err(e) = handle_client(&stream, &message) {
+                                    eprintln!("Error handling client: {}", e);
+                                }
+                            }
+                            _ => {
+                                eprintln!("Unknown command: {:?}", lines);
+                            }
                         }
                     }
                 });
