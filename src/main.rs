e68e740f828adeb5d3c4a1e78a573fe2b081fb5a
Jason Kim <actcrazy@gmail.com>
2024-07-28 22:30:37 +0900

    feat: 쓰레드 적용

--- src/main.rs
+++ src/main.rs
@@ -13,18 +13,20 @@ fn main() {
     for stream in listener.incoming() {
         match stream {
             Ok(mut stream) => {
-                println!("accepted new connection");
-                loop {
-                    let mut buf = [0; 1024];
-                    let size = stream.read(&mut buf).unwrap_or(0);
-                    if size == 0 {
-                        break;
-                    }
+                std::thread::spawn(move || {
+                    println!("accepted new connection");
+                    loop {
+                        let mut buf = [0; 1024];
+                        let size = stream.read(&mut buf).unwrap_or(0);
+                        if size == 0 {
+                            break;
+                        }
 
-                    if let Err(e) = handle_client(&stream, "+PONG\r\n") {
-                        eprintln!("Error handling client: {}", e);
+                        if let Err(e) = handle_client(&stream, "+PONG\r\n") {
+                            eprintln!("Error handling client: {}", e);
+                        }
                     }
-                }
+                });
             }
             Err(e) => {
                 println!("error: {}", e);
