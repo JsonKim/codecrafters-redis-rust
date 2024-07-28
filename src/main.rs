2c88c860eabf4f3e467efaf94c17368c4058fc0a
Jason Kim <actcrazy@gmail.com>
2024-07-28 22:27:50 +0900

    fix: stream을 여러번 읽을 수 있게 수정

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use std::io::{Error, Write};
+use std::io::{Error, Read, Write};
 use std::net::{TcpListener, TcpStream};
 
-fn handle_client(mut stream: TcpStream, message: &str) -> Result<(), Error> {
+fn handle_client(mut stream: &TcpStream, message: &str) -> Result<(), Error> {
     stream.write(message.as_bytes())?;
     stream.flush()?;
     Ok(())
@@ -12,10 +12,18 @@ fn main() {
 
     for stream in listener.incoming() {
         match stream {
-            Ok(stream) => {
+            Ok(mut stream) => {
                 println!("accepted new connection");
-                if let Err(e) = handle_client(stream, "+PONG\r\n") {
-                    eprintln!("Error handling client: {}", e);
+                loop {
+                    let mut buf = [0; 1024];
+                    let size = stream.read(&mut buf).unwrap_or(0);
+                    if size == 0 {
+                        break;
+                    }
+
+                    if let Err(e) = handle_client(&stream, "+PONG\r\n") {
+                        eprintln!("Error handling client: {}", e);
+                    }
                 }
             }
             Err(e) => {
