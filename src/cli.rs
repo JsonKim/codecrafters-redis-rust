5e91681f10f93e0ab42825e9c4de958c13d0cf0a
Jason Kim <actcrazy@gmail.com>
2024-07-30 00:53:05 +0900

    fix: --replicaof 파싱

--- src/cli.rs
+++ src/cli.rs
@@ -1,4 +1,26 @@
 use clap::{arg, Parser};
+use std::str::FromStr;
+
+#[derive(Debug, Clone)]
+pub struct ReplicaInfo {
+    pub host: String,
+    pub port: u16,
+}
+
+impl FromStr for ReplicaInfo {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let (host, port_str) = s.split_once(' ').ok_or("Invalid format. Use host:port")?;
+
+        let port = port_str.parse::<u16>().map_err(|_| "Invalid port number")?;
+
+        Ok(ReplicaInfo {
+            host: host.to_string(),
+            port,
+        })
+    }
+}
 
 #[derive(Parser, Debug)]
 pub struct Args {
@@ -6,7 +28,7 @@ pub struct Args {
     pub port: u16,
 
     #[arg(long)]
-    pub replicaof: Option<String>,
+    pub replicaof: Option<ReplicaInfo>,
 }
 
 pub fn parse_cli() -> Args {
